24020c801c69e5a99ebfd66ac89b2a83fe4ae9b0
Andrew-022 <145354381+Andrew-022@users.noreply.github.com>
2024-02-23 11:23:33 +0000

    category component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router'
 import { NavbarComponent } from "./navbar/navbar.component";
 import {AuthorDetailsComponent} from "./author-details/author-details.component";
 import {BookDetailsComponent} from "./book-details/book-details.component";
+import {CategoryComponent} from "./category/category.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, AuthorDetailsComponent, BookDetailsComponent],
+  imports: [RouterOutlet, NavbarComponent, AuthorDetailsComponent, BookDetailsComponent, CategoryComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
